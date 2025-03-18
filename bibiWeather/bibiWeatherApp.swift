ed3636ceb5df6791ce0f01352822eda1f688659c
Sam Liu <amazing_sam@outlook.com>
2025-03-18 21:16:54 +0800

    Initial Commit

---
+++ bibiWeather/bibiWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  bibiWeatherApp.swift
+//  bibiWeather
+//
+//  Created by 刘一笔 on 2025/3/18.
+//
+
+import SwiftUI
+
+@main
+struct bibiWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
